e446a7fdef5fe2e7788954d942312b46c98fec6c
shihyaolin <shihyaolin@tencent.com>
2021-04-21 10:55:57 -0700

    fix a typo

--- body_hands.py
+++ body_hands.py
@@ -28,10 +28,10 @@ while cap.isOpened():
 		for hand_landmarks in res_hand.multi_hand_landmarks:
 			mp_drawing.draw_landmarks(
 				annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-	# Render Detected Pose
+	# # Render Detected Pose
 	if res_body.pose_landmarks:
-		 (annotated_image, res_body.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-		mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+		mp_drawing.draw_landmarks(annotated_image, res_body.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+		mp_drawing.DrawingSpec(color=(245,117,66), thickness=5, circle_radius=5), 
 		mp_drawing.DrawingSpec(color=(245,66,230), thickness=5, circle_radius=5))	
 	cv2.imshow('Mediapipe Feed', cv2.flip(annotated_image,1))
 	if cv2.waitKey(10) & 0xFF == ord('q'):
